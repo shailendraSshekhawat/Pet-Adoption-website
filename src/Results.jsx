1b8a8694c3ef503ee7732eded67fca8cbf465718
Shailendra <shelbyquiz0@gmail.com>
2023-12-30 06:43:01 +0530

    before React Router

---
+++ src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            key={pet.id}
+            name={pet.name}
+            animal={pet.animal}
+            location={`${pet.city}, ${pet.state}`}
+            images={pet.images}
+            breed={pet.breed}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
