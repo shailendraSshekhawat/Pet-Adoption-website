2f99444c6019e37665e0c525d3d61175d72bfc26
Shailendra <shelbyquiz0@gmail.com>
2023-12-30 15:18:30 +0530

    added carousel class component just for fun

---
+++ src/Carousel.jsx
@@ -0,0 +1,43 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index, // + to convert index to number
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal hero" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              onClick={this.handleClick}
+              data-index={index}
+              src={photo}
+              key={photo}
+              alt="animal thumbnail"
+              className={index === active ? "active" : ""}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
